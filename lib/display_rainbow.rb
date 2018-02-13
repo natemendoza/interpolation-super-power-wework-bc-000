33ff92cf055c06cc84ea51ea73f8baa7df845be0
Nathaniel Mendoza <nathaniel.mendoza@wework.com>
2018-02-13 23:24:16 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,14 +1,14 @@
 # Write your #display_rainbow method here
-  rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
   
-def display_rainbow (rainbow)
+def display_rainbow (colors)
   puts colors[0]
   puts colors[1]
-  puts color[2]
-  puts color[3]
-  puts color[4]
-  puts color[5]
-  puts color[6]
+  puts colors[2]
+  puts colors[3]
+  puts colors[4]
+  puts colors[5]
+  puts colors[6]
 end
 
 display_rainbow
